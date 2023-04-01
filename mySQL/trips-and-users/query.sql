25609df9c15c0fd0da7d7ec614472802ed972785
marco <mgrazianodecastro@gmail.com>
2023-04-01 16:58:36 -0300

    mysql | trips-and-users

--- mySQL/trips-and-users/query.sql
+++ mySQL/trips-and-users/query.sql
@@ -28,35 +28,3 @@ JOIN (
 WHERE
     t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
 ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-SELECT
-    -- t.*
-    -- , c.banned      AS 'client_banned'
-    -- , d.banned      AS 'driver_banned'
-    COUNT(id),
-    request_at
-FROM Trips AS t
-LEFT JOIN Users AS c ON t.client_id = c.users_id
-LEFT JOIN Users AS d ON t.driver_id = d.users_id
-WHERE
-    (
-        c.banned != 'Yes' AND d.banned != 'Yes'
-    )
-    AND
-    (
-        t.status != "completed"
-    )
-GROUP BY request_at
-;
